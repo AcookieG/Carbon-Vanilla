01159dc903755a6dcee15af33ca0438bfc2de8e7
AcookieG <168602960+AcookieG@users.noreply.github.com>
2024-06-09 17:54:58 +0200

    turret updates

--- scripts/BlockBulletType.js
+++ scripts/BlockBulletType.js
@@ -1,4 +1,6 @@
-let block = Blocks.duo;
+//this is the variable name, check Blocks.java in core/scr/content in the mindustry github to check all of them
+
+let block = Blocks.router;
 
 let bullet = with(extend(BasicBulletType, {
   despawned(b){
@@ -6,5 +8,13 @@ let bullet = with(extend(BasicBulletType, {
     Vars.world.tileWorld(b.x, b.y).setBuild(block);
   }  
 })){
-  
+  //js object formatting here, more similar to hjson than json
+ collides: false 
 };
+//we need this cause js runs before json content loads, so this block allows us to find the turret after it loads
+Evenrs.on(ClientLoadEvent. e => {
+  //this gets your turret in js  
+let turret = Vars.content.getByName(carbon-vanilla-blockturret);
+  //ammoTypes is your turret's ammo. Put tells the game that this item makes the turret shoot this bullet
+  turret.ammoTypes.put(Items.pyratite, bullet);
+})
